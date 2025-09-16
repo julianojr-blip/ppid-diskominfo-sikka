384b8c3d7d9bc93ccc4c2a720e38fdc95b7de312
Juliano Jr <fifawordcup27@gmail.com>
2025-09-16 10:28:13 +0800

    Add App Router structure and login page

---
+++ app/login/page.tsx
@@ -0,0 +1,1 @@
+export default function LoginPage() { return <div><h1>Login Page</h1><form><input type="text" placeholder="Username" /><input type="password" placeholder="Password" /><button type="submit">Login</button></form></div>; } 
